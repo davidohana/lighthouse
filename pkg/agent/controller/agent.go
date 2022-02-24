058f5177daf0153ea0d3415648cf42ab11a2bf65
David Ohana <david.ohana@ibm.com>
2022-02-24 14:02:05 +0200

    log for service deletion 3
    Signed-off-by: David Ohana <david.ohana@ibm.com>

--- pkg/agent/controller/agent.go
+++ pkg/agent/controller/agent.go
@@ -513,7 +513,7 @@ func (a *Controller) serviceToRemoteServiceImport(obj runtime.Object, numRequeue
 	}
 
 	if !found {
-		// Service Export not created yet
+		klog.V(log.DEBUG).Infof("ServiceExport not found for deleted service: %s/%s", svc.Namespace, svc.Name, err
 		return nil, false
 	}
 
