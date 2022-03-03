fc4180046e1883ad9dbb06a481cd122d9d6cb244
David Ohana <david.ohana@ibm.com>
2022-03-03 15:59:24 +0200

    replace local client references from import syncer to slice syncer
    Signed-off-by: David Ohana <david.ohana@ibm.com>

--- pkg/agent/controller/cleanup.go
+++ pkg/agent/controller/cleanup.go
@@ -48,17 +48,17 @@ var (
 func (a *Controller) Cleanup() error {
 	// Delete all ServiceImports from the local cluster skipping those in the broker namespace if the broker is on the
 	// local cluster.
-	err := a.serviceImportSyncer.GetLocalClient().Resource(serviceImportGVR).Namespace(metav1.NamespaceAll).DeleteCollection(context.TODO(),
+	err := a.endpointSliceSyncer.GetLocalClient().Resource(serviceImportGVR).Namespace(metav1.NamespaceAll).DeleteCollection(context.TODO(),
 		metav1.DeleteOptions{},
 		metav1.ListOptions{
-			FieldSelector: fields.OneTermNotEqualSelector("metadata.namespace", a.serviceImportSyncer.GetBrokerNamespace()).String(),
+			FieldSelector: fields.OneTermNotEqualSelector("metadata.namespace", a.endpointSliceSyncer.GetBrokerNamespace()).String(),
 		})
 	if err != nil {
 		return errors.Wrap(err, "error deleting local ServiceImports")
 	}
 
 	// Delete all local ServiceImports from the broker.
-	err = a.serviceImportSyncer.GetBrokerClient().Resource(serviceImportGVR).Namespace(a.serviceImportSyncer.GetBrokerNamespace()).
+	err = a.endpointSliceSyncer.GetBrokerClient().Resource(serviceImportGVR).Namespace(a.endpointSliceSyncer.GetBrokerNamespace()).
 		DeleteCollection(context.TODO(), metav1.DeleteOptions{},
 			metav1.ListOptions{
 				LabelSelector: labels.Set(map[string]string{lhconstants.LighthouseLabelSourceCluster: a.clusterID}).String(),
@@ -72,7 +72,7 @@ func (a *Controller) Cleanup() error {
 	err = a.endpointSliceSyncer.GetLocalClient().Resource(endpointSliceGVR).Namespace(metav1.NamespaceAll).DeleteCollection(context.TODO(),
 		metav1.DeleteOptions{},
 		metav1.ListOptions{
-			FieldSelector: fields.OneTermNotEqualSelector("metadata.namespace", a.serviceImportSyncer.GetBrokerNamespace()).String(),
+			FieldSelector: fields.OneTermNotEqualSelector("metadata.namespace", a.endpointSliceSyncer.GetBrokerNamespace()).String(),
 			LabelSelector: labels.Set(map[string]string{discovery.LabelManagedBy: lhconstants.LabelValueManagedBy}).String(),
 		})
 	if err != nil {
